7336693daae0cc3483917f37846dcefbad6ab855
omaxx <maxx.orlov@gmail.com>
2022-01-21 16:08:46 -0500

    add .env add database authentication

--- netbook/db/__init__.py
+++ netbook/db/__init__.py
@@ -1,16 +1,19 @@
+import os
+
 import mongoengine as me
+
 from .models import *
 from .errors import *
 
 DB_HOST = "localhost"
 DB_NAME = "netbook"
-# DB_USER_NAME = "root"
-# DB_USER_PASSWORD = "root"
+DB_USER_NAME = os.environ.get("MONGO_USER_NAME")
+DB_USER_PASSWORD = os.environ.get("MONGO_USER_PASSWORD")
 
 me.connect(
     host=DB_HOST,
     db=DB_NAME,
-    # username=DB_USER_NAME,
-    # password=DB_USER_PASSWORD,
-    # authentication_source='admin',
+    username=DB_USER_NAME,
+    password=DB_USER_PASSWORD,
+    authentication_source='admin',
 )
--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+import dotenv
 from netbook.server import create_app
 
+dotenv.load_dotenv()
 create_app().run(debug=True)
